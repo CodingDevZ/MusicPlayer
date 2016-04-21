1089ae854069cb4e23056204238418d51944148e
CodingDevZ <sharpp999@gmail.com>
2016-04-21 19:38:02 +0200

    Media player

--- app/src/main/java/com/example/tomy_/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/tomy_/musicplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.tomy_.musicplayer;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,11 +23,24 @@ public class MainActivity extends AppCompatActivity {
     ListView lvPlaylist;
     String[] items;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean Theme1= preferences.getBoolean("Theme1", false);
+        boolean Theme2= preferences.getBoolean("Theme2", false);
+
+        if(Theme1) {
+            setTheme(R.style.Theme1);
+        }
+        else if(Theme2) {
+            setTheme(R.style.Theme2);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         initWidgets();
 
         final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
@@ -49,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
     private void initWidgets() {
 
         lvPlaylist = (ListView) findViewById(R.id.lvPlaylist);
+        Toolbar topToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+
 
     }
 
@@ -70,5 +91,33 @@ public class MainActivity extends AppCompatActivity {
         }
         return arrayList;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this,Preference.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
+
+
 
